f174b61be2815aee58754b83f80bff74a20a1eb7
Anthony Sim <anthonyysim@gmail.com>
2022-04-08 22:39:34 -0700

    footer complete

--- src/components/Additionals.tsx
+++ src/components/Additionals.tsx
@@ -1,7 +1,23 @@
+import '../styling/Additionals.scss';
+
 function Additionals() {
   return (
     <div>
-
+      <ul className="additionals">
+        <li className="additionals__item">About Value</li>|
+        <li className="additionals__item">Steamworks</li>|
+        <li className="additionals__item">Jobs</li>|
+        <li className="additionals__item">Steam Distribution</li>|
+        <li className="additionals__item">Gift Cards</li>|
+        <li className="additionals__item">
+          <img src="https://steam-fec.s3.amazonaws.com/images/ico_facebook.gif" alt="fb logo" />
+          <span>&nbsp;&nbsp;Steam</span>
+        </li>|
+        <li className="additionals__item">
+          <img src="https://steam-fec.s3.amazonaws.com/images/ico_twitter.gif" alt="twitter logo" />
+          <span>&nbsp;&nbsp;@steam</span>
+        </li>
+      </ul>
     </div>
   );
 }
